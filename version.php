31d21891a2db6a33fe057a874e24e076fdec79f5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-10-27 15:57:19 -0700

    bumps minimum required moodle version to v4, updates plugin version

--- version.php
+++ version.php
@@ -6,6 +6,6 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2021091400;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2020110300;        // Requires this Moodle version
+$plugin->version   = 2022102700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022041200;        // Requires this Moodle version
 $plugin->component = 'tool_ucsfsomapi';      // Full name of the plugin (used for diagnostics)
